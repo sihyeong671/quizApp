8f29c9022eca198c9acb0dd285ef368b50f79918
Giyeong Kim <ggcc503@gmail.com>
2022-01-06 17:17:45 +0900

    client: add signup page

--- client/lib/auth/login.dart
+++ client/lib/auth/login.dart
@@ -15,108 +15,106 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Log in'),
-        backgroundColor: Colors.redAccent,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: (){}
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: (){}
-          )
-        ]
-      ),
+      // appBar: AppBar(
+      //   title: Text('Log in'),
+      //   backgroundColor: Colors.redAccent,
+      //   centerTitle: true,
+      //   leading: IconButton(
+      //     icon: Icon(Icons.menu),
+      //     onPressed: (){}
+      //   ),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: Icon(Icons.search),
+      //       onPressed: (){}
+      //     )
+      //   ]
+      // ),
       body: Builder(
         builder: (context) {
-          return SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Padding(padding: EdgeInsets.only(top: 50)),
-                Form(
-                  child: Theme(
-                    data: ThemeData(
-                      primaryColor: Colors.teal,
-                      inputDecorationTheme: InputDecorationTheme(
-                        labelStyle: TextStyle(
-                          color: Colors.teal,
-                          fontSize: 15.0
+          return GestureDetector(
+            onTap: (){
+              FocusScope.of(context).unfocus();
+            },
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Padding(padding: EdgeInsets.only(top: 200)),
+                  Form(
+                    child: Theme(
+                      data: ThemeData(
+                        primaryColor: Colors.teal,
+                        inputDecorationTheme: InputDecorationTheme(
+                          labelStyle: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 15.0
+                          )
                         )
-                      )
-                    ),
-                    child: Container(
-                      padding: EdgeInsets.all(40.0),
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            controller: controller1,
-                            decoration: InputDecoration(
-                              labelText: 'ID'
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextField(
+                              controller: controller1,
+                              decoration: InputDecoration(
+                                labelText: 'ID',
+                                hintText: 'Enter your ID',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
                             ),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          TextField(
-                            controller: controller2,
-                            decoration: InputDecoration(
-                              labelText: 'PASSWORD'
+                            TextField(
+                              controller: controller2,
+                              decoration: InputDecoration(
+                                labelText: 'PASSWORD',
+                                hintText: 'Enter your PW',
+                              ),
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
                             ),
-                            keyboardType: TextInputType.text,
-                            obscureText: true,
-                          ),
-                          SizedBox(
-                            height: 40.0,
-                          ),
-                          ButtonTheme(
-                            minWidth: 100.0,
-                              height: 50.0,
-                              child: RaisedButton(
-                                color: Colors.orangeAccent,
-                                  child: Icon(
-                                    Icons.arrow_forward,
-                                  color: Colors.white,
-                                  size: 35.0,
-                                  ),
-                                  onPressed: (){
-        
-                                    if(controller1.text == 'test' && controller2.text == '1234'){
-                                      Navigator.push(context, 
-                                        MaterialPageRoute(builder: (BuildContext context) => Lobby()));
-                                    } else{
-                                      showSnackBar(context);
-                                    }
-        
-                                  },
-                              )
-                          ),
-                          ButtonTheme(
-                            minWidth: 100.0,
-                              height: 50.0,
-                              child: RaisedButton(
-                                color: Colors.orange,
-                                  child: Text(
-                                    "회원가입"
-                                  ),
-                                  onPressed: (){
-        
-                                    if(controller1.text == 'test' && controller2.text == '1234'){
+                            SizedBox(
+                              height: 40.0,
+                            ),
+                            ButtonTheme(
+                              minWidth: 100.0,
+                                height: 50.0,
+                                child: TextButton(
+                                    child: Icon(
+                                      Icons.arrow_forward,
+                                    size: 35.0,
+                                    ),
+                                    onPressed: (){
+                  
+                                      if(controller1.text == 'test' && controller2.text == '1234'){
+                                        Navigator.push(context, 
+                                          MaterialPageRoute(builder: (BuildContext context) => Lobby()));
+                                      } else{
+                                        showSnackBar(context);
+                                      }
+                  
+                                    },
+                                )
+                            ),
+                            ButtonTheme(
+                              minWidth: 100.0,
+                                height: 50.0,
+                                child: TextButton(
+                                    child: Text("회원가입"),
+                                    onPressed: (){
+
                                       Navigator.push(context, 
                                         MaterialPageRoute(builder: (BuildContext context) => SignUp()));
-                                    } else{
-                                      showSnackBar(context);
-                                    }
-        
-                                  },
-                              )
-                          )
-                        ]
+                                      
+                                    },
+                                )
+                            )
+                          ]
+                        )
                       )
                     )
                   )
-                )
-              ]
+                ]
+              ),
             ),
           );
         }
