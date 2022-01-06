7df0f8ba551d5e18a91a3ab708e78a59d3e771f1
sihyeong671 <bshlab671@naver.com>
2022-01-06 21:34:23 +0900

    server test finish

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
-require('dotenv').config()
+import dotenv from 'dotenv'
+import User from './model/user.js'
+// const User = require('./model/user');
+
+dotenv.config()
 
 const {PORT} = process.env
 
@@ -10,18 +14,50 @@ app.use(cors())
 app.use(express.json()) //body-parser와 동일
 
 // DB
-// mongoose.connect(
-//   'mongodb://localhost/test',
-//   {
-//     useNewURLParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopolgy: true
-//   }
-// )
+mongoose.connect('mongodb://localhost:27017/test')
+  .then(()=>console.log("Connected to MongoDB"))
+  .catch((err)=> console.log(err))
+
+// 회원가입
+app.post("/user", async (req, res) => {
+  console.log(req.body)
+  const user = new User(req.body);
+  console.log('연결성공')
+  try{
+    await user.save();
+    res.status(204).send()
+  }catch(e){
+    res.status(500).json({
+      message: "User 저장 실패"
+    })
+  }
+})
+
+// 회원 탈퇴
 
 
-// app.get('/', (req, res) => {
-//   res.send('Hello')
-// })
+
+// 유저 랭킹 보여주기
+app.get("/user", async(req, res) => {
+  try{
+    const users = await User.find({});
+
+    res.status(200).send(users);
+  }catch(e){
+    res.status(500).json({
+      message: "User 조회 실패"
+    })
+  }
+})
+
+// 유저 닉네임 수정
+app.patch("/user",  async(req, res) => {
+  try{
+
+  }catch(e){
+
+  }
+})
+
 
 app.listen(PORT, (err) => console.log(err))
---
+++ server/model/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import validator from "express-validator";
+
+const UserSchema = new mongoose.Schema({
+    nickName:{
+        type: String,
+        trim: true,
+        unique: true
+    },
+    // img:{
+    //     data: Buffer,
+    //     contentType: String
+    // },
+    createdDay: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = mongoose.model("User", UserSchema)
+
+// module.exports = User;
+
+export default User;
+
