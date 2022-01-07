634889783584db15d258d8a2841da63549b6099c
Giyeong Kim <ggcc503@gmail.com>
2022-01-07 17:54:16 +0900

    client: fix chatting in ingame page

--- client/lib/inGame/message.dart
+++ client/lib/inGame/message.dart
@@ -1,7 +1,6 @@
 import 'package:client/inGame/ChatMessage.dart';
 import 'package:flutter/material.dart';
 
-import 'text_message.dart';
 
 class Message extends StatelessWidget {
   const Message({
@@ -13,15 +12,6 @@ class Message extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget messageContaint(ChatMessage message) {
-      switch (message.messageType) {
-        case ChatMessageType.text:
-          return TextMessage(message: message);
-        default:
-          return SizedBox();
-      }
-    }
-
     return Padding(
       padding: const EdgeInsets.only(top: 20.0),
       child: Row(
@@ -29,51 +19,16 @@ class Message extends StatelessWidget {
             message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
         children: [
           if (!message.isSender) ...[
+            SizedBox(width: 20.0 / 2),
             CircleAvatar(
               radius: 12,
               backgroundImage: AssetImage("assets/cutesexy.jpeg"),
             ),
             SizedBox(width: 20.0 / 2),
           ],
-          messageContaint(message),
-          if (message.isSender) MessageStatusDot(status: message.messageStatus)
+          Text(message.text),
+          SizedBox(width: 20.0 / 2),
         ],
-      ),
-    );
-  }
-}
-
-class MessageStatusDot extends StatelessWidget {
-  final MessageStatus? status;
-
-  const MessageStatusDot({Key? key, this.status}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    Color dotColor(MessageStatus status) {
-      switch (status) {
-        case MessageStatus.not_sent:
-          return Colors.black;
-        case MessageStatus.not_view:
-          return Theme.of(context).textTheme.bodyText1!.color!.withOpacity(0.1);
-        case MessageStatus.viewed:
-          return Colors.black;
-        default:
-          return Colors.transparent;
-      }
-    }
-
-    return Container(
-      margin: EdgeInsets.only(left: 20.0 / 2),
-      height: 12,
-      width: 12,
-      decoration: BoxDecoration(
-        color: dotColor(status!),
-        shape: BoxShape.circle,
-      ),
-      child: Icon(
-        status == MessageStatus.not_sent ? Icons.close : Icons.done,
-        size: 8,
-        color: Theme.of(context).scaffoldBackgroundColor,
       ),
     );
   }
