6338c01deef6b5c43db64171dae04803f2853d30
Giyeong Kim <ggcc503@gmail.com>
2022-01-10 02:08:18 +0900

    client: remove login text field in login page

--- client/lib/auth/login.dart
+++ client/lib/auth/login.dart
@@ -49,91 +49,95 @@ class _LogInState extends State<LogIn> {
               FocusScope.of(context).unfocus();
             },
             child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.only(top: 200)),
-                  Form(
-                    child: Theme(
-                      data: ThemeData(
-                        primaryColor: Colors.teal,
-                        inputDecorationTheme: InputDecorationTheme(
-                          labelStyle: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 15.0
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.only(top: 200)),
+                    Form(
+                      child: Theme(
+                        data: ThemeData(
+                          primaryColor: Colors.teal,
+                          inputDecorationTheme: InputDecorationTheme(
+                            labelStyle: TextStyle(
+                              color: Colors.teal,
+                              fontSize: 15.0
+                            )
                           )
-                        )
-                      ),
-                      child: Container(
-                        padding: EdgeInsets.all(40.0),
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(
-                              height: 40.0,
-                            ),
-                            ButtonTheme(
-                              minWidth: 100.0,
-                              height: 50.0,
-                              child: ElevatedButton(
-                                child: Text('게스트 로그인'),
-                                onPressed: () async {
-                                    final response = await http.get(Uri.parse("https://nickname.hwanmoo.kr/?format=json&count=1"));
-                                    final jsonData = jsonDecode(response.body);
-                                    var name = jsonData['words'][0];
-                                    var url = "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80";
-                                    
-                                    provider.add("0", name, url, 0, true);
-                                    Navigator.push(context, 
-                                      MaterialPageRoute(builder: (BuildContext context) => MyPage()));
-                                },
-                              )
-                            ),
-                            ButtonTheme(
-                              minWidth: 100.0,
-                                height: 50.0,
-                                child: TextButton(
-                                    child: Image.asset('assets/kakao_login.png'),
-                                    onPressed: () async {
-                                      try {
-                                        final token = _isKaKaoTalkInstalled
-                                        ? await UserApi.instance.loginWithKakaoTalk() 
-                                        : await UserApi.instance.loginWithKakaoAccount();
-                                        
-                                        // print(UserApi.instance.accessTokenInfo().toString());
-                                        User kakaoUser = await UserApi.instance.me();
-                                        var userID = kakaoUser.id.toString();
-                                        var name = kakaoUser.properties!['nickname'].toString();
-                                        var image = kakaoUser.properties!['thumbnail_image'];
-                                        // var res;
-
-                                        try{
-                                          final res = await http.get(Uri.parse('http://10.0.2.2:8080/user/${userID}'));
-                                          final jsonData = jsonDecode(res.body);
-                                          provider.add(jsonData[0]['userID'], jsonData[0]['nickName'], jsonData[0]['img'], jsonData[0]['score'], false);
-                                        } catch(e){
-                                          final res = await http.post(Uri.parse('http://10.0.2.2:8080/user/save'),
-                                            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                                            body: {"userID": userID, "nickName": name, "img": image});
-                                          final jsonData = jsonDecode(res.body);
-                                          provider.add(jsonData[0]['userID'], jsonData[0]['nickName'], jsonData[0]['img'], jsonData[0]['score'], false);
-                                        }
-                                        
-
-                                        
-                                        
-                                        Navigator.push(context, 
-                                          MaterialPageRoute(builder: (BuildContext context) => MyPage()));
-                                      } catch (e) {
-                                        print('error on login: $e');
-                                      }
-                                    },
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.all(40.0),
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 100.0,
+                              ),
+                              // Image.asset('assets/quiz_icon.png'),
+                              SizedBox(
+                                width: 296.0,
+                                height: 42.0,
+                                child: ElevatedButton.icon(
+                                  icon: Icon(Icons.add, size: 18),
+                                  label: Text('게스트 로그인'),
+                                  onPressed: () async {
+                                      final response = await http.get(Uri.parse("https://nickname.hwanmoo.kr/?format=json&count=1"));
+                                      final jsonData = jsonDecode(response.body);
+                                      var name = jsonData['words'][0];
+                                      var url = "https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80";
+                                      
+                                      provider.add("0", name, url, 0, true);
+                                      Navigator.push(context, 
+                                        MaterialPageRoute(builder: (BuildContext context) => MyPage()));
+                                  },
                                 )
-                            ),
-                          ]
+                              ),
+                              ButtonTheme(
+                                minWidth: 100.0,
+                                  height: 50.0,
+                                  child: TextButton(
+                                      child: Image.asset('assets/kakao_login_wide.png'),
+                                      onPressed: () async {
+                                        try {
+                                          final token = _isKaKaoTalkInstalled
+                                          ? await UserApi.instance.loginWithKakaoTalk() 
+                                          : await UserApi.instance.loginWithKakaoAccount();
+                                          
+                                          // print(UserApi.instance.accessTokenInfo().toString());
+                                          User kakaoUser = await UserApi.instance.me();
+                                          var userID = kakaoUser.id.toString();
+                                          var name = kakaoUser.properties!['nickname'].toString();
+                                          var image = kakaoUser.properties!['thumbnail_image'];
+                                          // var res;
+
+                                          try{
+                                            final res = await http.get(Uri.parse('http://10.0.2.2:8080/user/${userID}'));
+                                            final jsonData = jsonDecode(res.body);
+                                            provider.add(jsonData[0]['userID'], jsonData[0]['nickName'], jsonData[0]['img'], jsonData[0]['score'], false);
+                                          } catch(e){
+                                            final res = await http.post(Uri.parse('http://10.0.2.2:8080/user/save'),
+                                              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                                              body: {"userID": userID, "nickName": name, "img": image});
+                                            final jsonData = jsonDecode(res.body);
+                                            provider.add(jsonData[0]['userID'], jsonData[0]['nickName'], jsonData[0]['img'], jsonData[0]['score'], false);
+                                          }
+                                          
+
+                                          
+                                          
+                                          Navigator.push(context, 
+                                            MaterialPageRoute(builder: (BuildContext context) => MyPage()));
+                                        } catch (e) {
+                                          print('error on login: $e');
+                                        }
+                                      },
+                                  )
+                              ),
+                            ]
+                          )
                         )
                       )
                     )
-                  )
-                ]
+                  ]
+                ),
               ),
             ),
           );
